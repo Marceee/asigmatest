5833a2cf92cb3e3c796226798415fe29d6ea9a17
marceee <marcy@elsa.science>
2024-10-17 08:57:32 +0300

    basic template

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {Alert, Button, Container, Navbar} from "react-bootstrap";
 
 function App() {
+
+    const [count, setCount] = React.useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand href="#home">Test</Navbar.Brand>
+          </Container>
+        </Navbar>
+
+        {/* Main Content */}
+        <Container className="mt-4">
+          <h1>TEST TSET TEST</h1>
+          <p>Hit the ground running</p>
+            <p className={"space"}>{count}</p>
+
+            <div>
+                <Button onClick={()=>{
+                setCount(count + 1)
+            }} variant="primary" className={"me-2"}>Max</Button>
+                <Button onClick={()=>{
+                    setCount(count - 1)
+                }} variant="primary">Min</Button>
+            </div>
+
+
+            <Alert dismissible variant="danger" className={"space"}>
+                <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
+                <p>Change this and that and try again.</p>
+            </Alert>
+
+
+        </Container>
+      </div>
   );
 }
 
